f46cca7f4c8cf91234e0804e1da9a094314fa922
Benii <benii.alco@gmail.com>
2022-12-04 15:05:13 +0000

    automated file creation now that we seem to have a pattern to follow!

---
+++ src/day_19.rs
@@ -0,0 +1,43 @@
+pub fn run() {
+let input = include_str!("input/day_19");
+println!("{}", puzzle_1(input));
+println!("{}", puzzle_2(input));
+}
+
+// 
+
+fn parse_input(input: &str) -> Vec<u64> {
+unimplemented!()
+}
+
+// 
+fn puzzle_1(input: &str) -> u64 {
+unimplemented!()
+}
+
+// 
+fn puzzle_2(input: &str) -> u64 {
+unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+use super::*;
+
+const TEST_INPUT: &str = "";
+
+#[test]
+fn day_19_parse_input() {
+assert_eq!(parse_input(TEST_INPUT), vec![0]);
+}
+
+#[test]
+fn day_19_puzzle_1() {
+assert_eq!(puzzle_1(TEST_INPUT), 0);
+}
+
+#[test]
+fn day_19_puzzle_2() {
+assert_eq!(puzzle_2(TEST_INPUT), 0);
+}
+}
