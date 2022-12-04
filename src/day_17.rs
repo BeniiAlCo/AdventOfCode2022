6cdd57a4ed9a5a408915fc54d57263124254e511
Benii <benii.alco@gmail.com>
2022-12-04 15:07:08 +0000

    ... cleaning up some issues with the automated files

--- src/day_17.rs
+++ src/day_17.rs
@@ -1,43 +1,43 @@
 pub fn run() {
-let input = include_str!("input/day_17");
-println!("{}", puzzle_1(input));
-println!("{}", puzzle_2(input));
+	//let input = include_str!("input/day_17");
+	println!("{}", puzzle_1(input));
+	println!("{}", puzzle_2(input));
 }
 
 // 
 
 fn parse_input(input: &str) -> Vec<u64> {
-unimplemented!()
+	unimplemented!()
 }
 
 // 
 fn puzzle_1(input: &str) -> u64 {
-unimplemented!()
+	unimplemented!()
 }
 
 // 
 fn puzzle_2(input: &str) -> u64 {
-unimplemented!()
+	unimplemented!()
 }
 
 #[cfg(test)]
 mod tests {
-use super::*;
+	use super::*;
 
-const TEST_INPUT: &str = "";
+	const TEST_INPUT: &str = "";
 
-#[test]
-fn day_17_parse_input() {
-assert_eq!(parse_input(TEST_INPUT), vec![0]);
-}
+	#[test]
+	fn day_17_parse_input() {
+		assert_eq!(parse_input(TEST_INPUT), vec![0]);
+	}
 
-#[test]
-fn day_17_puzzle_1() {
-assert_eq!(puzzle_1(TEST_INPUT), 0);
-}
+	#[test]
+	fn day_17_puzzle_1() {
+		assert_eq!(puzzle_1(TEST_INPUT), 0);
+	}
 
-#[test]
-fn day_17_puzzle_2() {
-assert_eq!(puzzle_2(TEST_INPUT), 0);
-}
+	#[test]
+	fn day_17_puzzle_2() {
+		assert_eq!(puzzle_2(TEST_INPUT), 0);
+	}
 }
